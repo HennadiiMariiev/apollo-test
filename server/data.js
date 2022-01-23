1f2380219f1f0aa174b4fee5f92e45fb879a1c98
Hennadii_Mariiev <marieff1990@gmail.com>
2022-01-23 22:20:51 +0200

    add server logic

---
+++ server/data.js
@@ -0,0 +1,70 @@
+const users = [
+  {
+    id: 1,
+    username: 'J. K. Rowling',
+    age: 24,
+  },
+  {
+    id: 2,
+    username: 'J. R. R. Tolkien',
+    age: 42,
+  },
+  {
+    id: 3,
+    username: 'Ken Kizzi',
+    age: 50,
+  },
+];
+
+const books = [
+  {
+    id: 1,
+    name: 'Harry Potter and the Chamber of Secrets',
+    pages: 423,
+    authorId: 1,
+  },
+  {
+    id: 2,
+    name: 'Harry Potter and the Prizoner of Azkaban',
+    pages: 399,
+    authorId: 1,
+  },
+  {
+    id: 3,
+    name: 'Harry Potter and the Goblet of Fire',
+    pages: 408,
+    authorId: 1,
+  },
+  {
+    id: 4,
+    name: 'The Fellowship of the Ring',
+    pages: 280,
+    authorId: 2,
+  },
+  {
+    id: 5,
+    name: 'The Two Towers',
+    pages: 340,
+    authorId: 2,
+  },
+  {
+    id: 6,
+    name: 'The Return of the King',
+    pages: 411,
+    authorId: 2,
+  },
+  {
+    id: 7,
+    name: 'Flying over the cooco`s nest',
+    pages: 223,
+    authorId: 3,
+  },
+  {
+    id: 8,
+    name: 'The three oranges',
+    pages: 205,
+    authorId: 3,
+  },
+];
+
+module.exports = { users, books };
---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const { graphqlHTTP } = require('express-graphql');
+const { schema } = require('./schema');
+const { users } = require('./data');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+
+const createUser = (input) => {
+  const id = Date.now();
+  return {
+    id,
+    ...input,
+  };
+};
+
+const root = {
+  getAllUsers: () => {
+    return users;
+  },
+  getUser: ({ id }) => {
+    return users.find((user) => user.id === id);
+  },
+  createUser: ({ input }) => {
+    const user = createUser(input);
+    users.push(user);
+    return user;
+  },
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+    schema: schema,
+    rootValue: root,
+  })
+);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
